b12e1d42295a64fca7f6024e8cf2cd821b6f2349
Marilaine Medeiros <marilainemedeiros12@gmail.com>
2025-02-28 20:03:24 -0300

    Exercicio6

---
+++ src/Exercicio6.java
@@ -0,0 +1,20 @@
+//Desenvolva um algoritmo em Java que leia um número inteiro e imprima o seu antecessor e seu sucessor.
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int antecessor = numero - 1;
+        int sucessor = numero + 1;
+
+        System.out.println("O antecessor de " + numero + " é: " + antecessor);
+        System.out.println("O sucessor de " + numero + " é: " + sucessor);
+
+        scanner.close();
+    }
+}
